3fe4882cdb80e8598a33d119d46ee0b635cdefde
cbontems <67456035+cbontems@users.noreply.github.com>
2022-07-07 20:52:49 +0200

    Uninstall deletes DB records

--- uninstall.php
+++ uninstall.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * OxyProps Lite
+ * OxyProps Lite.
  *
- * @link              https://oxyprops.com
+ * @see              https://oxyprops.com
  * @since             1.0.0
- * @package           OxyProps_Lite
- * 
- * This file triggers on plugin uninstall
  */
-
  if (!defined('WP_UNINSTALL_PLUGIN')) {
-    die;
+     exit;
  }
 
- //  Do stuff like clear database stored data via SQL
- //  global $wpdb;
- //  $wpdb->query("DELETE FROM wp_posts WHERE post_type= 'books'");
- //  $wpdb->query("DELETE FROM wp_postsmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)");
- //  $wpdb->query("DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)");
+$oxypropsLiteOptions = [
+    'oxyprops_lite',
+    'oxyprops_lite_packages',
+];
+
+// Clean up after us
+ foreach ($oxypropsLiteOptions as $option) {
+     delete_option($option);
+ }
