8d5a80615a2ed090332754bc5883a6a084c0b3d7
cbontems <67456035+cbontems@users.noreply.github.com>
2022-07-18 12:23:10 +0200

    Renamed folders

--- includes/Init.php
+++ includes/Init.php
@@ -19,12 +19,10 @@ final class Init
     public static function getServices()
     {
         return [
-            // Base\Helpers::class,
-            Pages\Dashboard::class,
-            Pages\IndividualPackages::class,
+            Dashboard\Dashboard::class,
+            Dashboard\IndividualPackages::class,
             Api\Callbacks\AdminCallbacks::class,
             Api\Callbacks\FieldsCallbacks::class,
-            // Api\SettingsApi::class,
             Base\Enqueue::class,
             Base\Gutenberg::class,
             Base\SettingsLinks::class,
