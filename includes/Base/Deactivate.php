dc39eb18c47547c331c227763f198eae05974ee8
cbontems <67456035+cbontems@users.noreply.github.com>
2022-07-18 14:08:55 +0200

    Proper comments

--- includes/Base/Deactivate.php
+++ includes/Base/Deactivate.php
@@ -1,16 +1,38 @@
 <?php
-
 /**
- * OxyProps Lite.
+ * Deactivate
+ * Manages plugin deactivation.
+ * php version 7.4.29
  *
- * @see              https://lite.oxyprops.com
- * @since             1.0.0
+ * @category Base
+ * @package  OxyPropsLite
+ * @author   Cédric Bontems <dev@oxyprops.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://lite.oxyprops.com OxyProps Lite Website
+ * @since    1.0.0
  */
 
 namespace Inc\Base;
 
+/**
+ * Deactivate Class
+ * Manages plugin deactivation.
+ * php version 7.4.29
+ *
+ * @category Base
+ * @package  OxyPropsLite
+ * @author   Cédric Bontems <dev@oxyprops.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://lite.oxyprops.com OxyProps Lite Website
+ * @since    1.0.0
+ */
 class Deactivate
 {
+    /**
+     * Recreate rewrite rules after plugin deactivation
+     *
+     * @return void
+     */
     public static function deactivate()
     {
         flush_rewrite_rules();
