9b412fe5fa10574650447c351a0857f103c3a7d6
cbontems <67456035+cbontems@users.noreply.github.com>
2022-07-18 12:20:46 +0200

    Refined Postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,36 @@
+const postcssPresetEnv = require("postcss-preset-env");
+const postcssImport = require("postcss-import");
+const cssnano = require("cssnano");
+const combineSelectors = require("postcss-combine-duplicated-selectors");
+const autoprefixer = require("autoprefixer");
+
+const lib = process.env.npm_lifecycle_event;
+
+const inlineMediaQueries = lib === "lib:media" || lib === "lib:supports";
+// todo: inline MQs for 'lib:all' when it's supported better
+
 module.exports = {
-    plugins: [
-      ["cssnano", {}],
-      ["autoprefixer", {}],
-      ["pixrem", {}],
-      ["postcss-import", {}]
-    ],
-  };
+  plugins: [
+    postcssImport(),
+    postcssPresetEnv({
+      stage: 0,
+      autoprefixer: false,
+      features: {
+        "logical-properties-and-values": false,
+        "prefers-color-scheme-query": false,
+        "gap-properties": false,
+        "custom-properties": false,
+        "place-properties": false,
+        "not-pseudo-class": false,
+        "focus-visible-pseudo-class": false,
+        "focus-within-pseudo-class": false,
+        "color-functional-notation": false,
+        "custom-media-queries": { preserve: inlineMediaQueries },
+      },
+    }),
+    combineSelectors(),
+    cssnano({
+      preset: "default",
+    }),
+  ],
+};
