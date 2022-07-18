bc0a74b0c19b02404d34079f0e404f0c2ccc5cc0
cbontems <67456035+cbontems@users.noreply.github.com>
2022-07-18 12:26:59 +0200

    Added Docs and Go Pro Links

--- includes/Base/SettingsLinks.php
+++ includes/Base/SettingsLinks.php
@@ -27,10 +27,18 @@ class SettingsLinks extends BaseController
         add_filter("plugin_action_links_{$this->plugin}", [$this, 'settingsLinks']);
     }
 
+    /**
+     * Adds links to the plugins page.
+     *
+     * @param array $links array of plugin links
+     *
+     * @return array
+     */
     public function settingsLinks($links)
     {
-        $settings_links = '<a href="admin.php?page=oxyprops_lite">Settings</a>';
-        array_push($links, $settings_links);
+        $links[] = '<a href="'.esc_url(admin_url('admin.php?page=oxyprops_lite')).'">'.esc_html__('Settings', 'oxyprops_lite').'</a>';
+        $links[] = '<a target="_blank" href="https://docs.oxyprops.com" >'.esc_html__('Docs', 'oxyprops_lite').'</a>';
+        $links[] = '<a target="_blank" href="https://oxyprops.com/shop/" style="color: var(--o-green-6); font-weight: bold">'.esc_html__('Go Pro', 'oxyprops_lite').'</a>';
 
         return $links;
     }
