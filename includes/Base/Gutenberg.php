2299dca1d0f13c44ae011364c7508aba5f7813c0
cbontems <67456035+cbontems@users.noreply.github.com>
2022-08-14 23:36:58 +0200

    Code formatting

--- includes/Base/Gutenberg.php
+++ includes/Base/Gutenberg.php
@@ -17,76 +17,68 @@ namespace Inc\Base;
 /**
  * Gutenberg Class
  * Manages interactions with Gutenberg.
- * php version 7.4.29
  *
- * @category Base
- * @package  OxyPropsLite
  * @author   Cédric Bontems <dev@oxyprops.com>
- * @license  https://opensource.org/licenses/MIT MIT
- * @link     https://lite.oxyprops.com OxyProps Lite Website
  * @since    1.0.0
  */
-class Gutenberg extends BaseController
-{
-    /**
-     * Stores the Gutenberg Singleton.
-     *
-     * @var object
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    private static $_instance;
+class Gutenberg extends BaseController {
 
-    /**
-     * Returns the Gutenberg Singleton.
-     *
-     * @return object Instance
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public static function getInstance()
-    {
-        if (null === self::$_instance) {
-            self::$_instance = new Gutenberg();
-        }
+	/**
+	 * Stores the Gutenberg Singleton.
+	 *
+	 * @var object
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	private static $instance;
 
-        return self::$_instance;
-    }
+	/**
+	 * Returns the Gutenberg Singleton.
+	 *
+	 * @return object Instance
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public static function get_instance() {
+		if ( null === self::$instance ) {
+			self::$instance = new Gutenberg();
+		}
 
-    /**
-     * Initializes the Gutenberg Class
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public function register()
-    {
-        if (isset(get_option('oxyprops_lite')['oxyprops_lite_gutenberg_fix'])) {
-            if (get_option('oxyprops_lite')['oxyprops_lite_gutenberg_fix']) {
-                self::dequeueGutenbergGloablStyles();
-            }
-        }
-    }
+		return self::$instance;
+	}
 
-    /**
-     * Removes Gutenberg default styles and SVG presets
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public function dequeueGutenbergGloablStyles()
-    {
-        // Remove WP CSS variables
-        remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
-        remove_action('wp_footer', 'wp_enqueue_global_styles', 1);
+	/**
+	 * Initializes the Gutenberg Class
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public function register() {
+		if ( isset( get_option( 'oxyprops_lite' )['oxyprops_lite_gutenberg_fix'] ) ) {
+			if ( get_option( 'oxyprops_lite' )['oxyprops_lite_gutenberg_fix'] ) {
+				self::dequeueGutenbergGloablStyles();
+			}
+		}
+	}
 
-        // Remove WP SVG definitions
-        remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
-    }
+	/**
+	 * Removes Gutenberg default styles and SVG presets
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public function dequeueGutenbergGloablStyles() {
+		// Remove WP CSS variables.
+		remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
+		remove_action( 'wp_footer', 'wp_enqueue_global_styles', 1 );
+
+		// Remove WP SVG definitions.
+		remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
+	}
 }
