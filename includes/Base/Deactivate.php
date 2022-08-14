09b5f5cec3926dae0b536b2dc7edfded4e7c4201
cbontems <67456035+cbontems@users.noreply.github.com>
2022-08-14 23:36:12 +0200

    Code formatting

--- includes/Base/Deactivate.php
+++ includes/Base/Deactivate.php
@@ -17,24 +17,18 @@ namespace Inc\Base;
 /**
  * Deactivate Class
  * Manages plugin deactivation.
- * php version 7.4.29
  *
- * @category Base
- * @package  OxyPropsLite
  * @author   Cédric Bontems <dev@oxyprops.com>
- * @license  https://opensource.org/licenses/MIT MIT
- * @link     https://lite.oxyprops.com OxyProps Lite Website
  * @since    1.0.0
  */
-class Deactivate
-{
-    /**
-     * Recreate rewrite rules after plugin deactivation
-     *
-     * @return void
-     */
-    public static function deactivate()
-    {
-        flush_rewrite_rules();
-    }
+class Deactivate {
+
+	/**
+	 * Recreate rewrite rules after plugin deactivation
+	 *
+	 * @return void
+	 */
+	public static function deactivate() {
+		flush_rewrite_rules();
+	}
 }
