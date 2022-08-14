8191bbaef0ce05528e8f10e9663104ad7f11ecad
cbontems <67456035+cbontems@users.noreply.github.com>
2022-08-14 23:35:23 +0200

    Code formatting

--- includes/Base/Activate.php
+++ includes/Base/Activate.php
@@ -17,73 +17,65 @@ namespace Inc\Base;
 /**
  * Activate Class
  * Manages plugin activation.
- * php version 7.4.29
  *
- * @category Base
- * @package  OxyPropsLite
  * @author   Cédric Bontems <dev@oxyprops.com>
- * @license  https://opensource.org/licenses/MIT MIT
- * @link     https://lite.oxyprops.com OxyProps Lite Website
  * @since    1.0.0
  */
-class Activate
-{
-    /**
-     * Sets default settings during plugin activation
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public static function activate()
-    {
-        self::_setDefaultParameters();
-        self::_setDefaultPackages();
-        flush_rewrite_rules();
-    }
+class Activate {
 
-    /**
-     * Registers default plugin settings in the database
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    private static function _setDefaultParameters()
-    {
-        if (get_option('oxyprops_lite')) {
-            return;
-        }
-        $default = [];
-        $oxyprops_lite_options = Helpers::getPluginDefaultOptions();
-        foreach ($oxyprops_lite_options as $key => $parameters) {
-            $default[$key] = $parameters['default'];
-        }
+	/**
+	 * Sets default settings during plugin activation
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public static function activate() {
+		self::set_default_parameters();
+		self::set_default_packages();
+		flush_rewrite_rules();
+	}
 
-        update_option('oxyprops_lite', $default);
-    }
+	/**
+	 * Registers default plugin settings in the database
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	private static function set_default_parameters() {
+		if ( get_option( 'oxyprops_lite' ) ) {
+			return;
+		}
+		$default               = array();
+		$oxyprops_lite_options = Helpers::get_plugin_default_options();
+		foreach ( $oxyprops_lite_options as $key => $parameters ) {
+			$default[ $key ] = $parameters['default'];
+		}
 
-    /**
-     * Registers default packages settings in the database
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    private static function _setDefaultPackages()
-    {
-        if (get_option('oxyprops_lite_packages')) {
-            return;
-        }
-        $default = [];
-        $oxyprops_lite_options = Helpers::getPluginPackagesOptions();
-        foreach ($oxyprops_lite_options as $key => $parameters) {
-            $default[$key] = $parameters['default'];
-        }
+		update_option( 'oxyprops_lite', $default );
+	}
 
-        update_option('oxyprops_lite_packages', $default);
-    }
+	/**
+	 * Registers default packages settings in the database
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	private static function set_default_packages() {
+		if ( get_option( 'oxyprops_lite_packages' ) ) {
+			return;
+		}
+		$default               = array();
+		$oxyprops_lite_options = Helpers::get_plugin_packages_options();
+		foreach ( $oxyprops_lite_options as $key => $parameters ) {
+			$default[ $key ] = $parameters['default'];
+		}
+
+		update_option( 'oxyprops_lite_packages', $default );
+	}
 }
