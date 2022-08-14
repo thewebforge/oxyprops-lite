7666c9d93fdea81d557db5d25b36b0ab688041d8
cbontems <67456035+cbontems@users.noreply.github.com>
2022-08-14 23:38:46 +0200

    Code formatting

--- includes/Init.php
+++ includes/Init.php
@@ -17,54 +17,47 @@ namespace Inc;
 /**
  * Init Class
  * Initializes the plugin.
- * php version 7.4.29
  *
- * @category Base
- * @package  OxyPropsLite
  * @author   Cédric Bontems <dev@oxyprops.com>
- * @license  https://opensource.org/licenses/MIT MIT
- * @link     https://lite.oxyprops.com OxyProps Lite Website
  * @since    1.0.0
  */
-final class Init
-{
-    /**
-     * Stores all the classes to initialize in an array.
-     *
-     * @return array full list of classes
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public static function getServices()
-    {
-        return [
-            Base\Enqueue::class,
-            Dashboard\Dashboard::class,
-            Dashboard\IndividualPackages::class,
-            Api\Callbacks\AdminCallbacks::class,
-            Api\Callbacks\FieldsCallbacks::class,
-            Base\Gutenberg::class,
-            Base\SettingsLinks::class,
-        ];
-    }
+final class Init {
 
-    /**
-     * Loop through the classes, initialize them, and
-     * call the register() method if it exists.
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public static function registerServices()
-    {
-        foreach (self::getServices() as $class) {
-            $service = $class::getInstance();
-            if (method_exists($service, 'register')) {
-                $service->register();
-            }
-        }
-    }
+	/**
+	 * Stores all the classes to initialize in an array.
+	 *
+	 * @return array full list of classes
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public static function get_services() {
+		return array(
+			Base\Enqueue::class,
+			Dashboard\Dashboard::class,
+			Dashboard\IndividualPackages::class,
+			Api\Callbacks\AdminCallbacks::class,
+			Api\Callbacks\FieldsCallbacks::class,
+			Base\Gutenberg::class,
+			Base\SettingsLinks::class,
+		);
+	}
+
+	/**
+	 * Loop through the classes, initialize them, and
+	 * call the register() method if it exists.
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public static function register_services() {
+		foreach ( self::get_services() as $class ) {
+			$service = $class::get_instance();
+			if ( method_exists( $service, 'register' ) ) {
+				$service->register();
+			}
+		}
+	}
 }
