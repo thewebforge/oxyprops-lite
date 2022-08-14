f056558c610c65714f44490ba33e0da74ad84024
cbontems <67456035+cbontems@users.noreply.github.com>
2022-08-14 23:38:29 +0200

    Code formatting and i18n friendly

--- includes/Dashboard/Dashboard.php
+++ includes/Dashboard/Dashboard.php
@@ -18,259 +18,270 @@ use Inc\Api\Callbacks\AdminCallbacks;
 use Inc\Api\Callbacks\FieldsCallbacks;
 use Inc\Api\SettingsApi;
 use Inc\Base\BaseController;
+use Inc\Base\Options;
 
 /**
  * Dashboard Class
  * Creates OxyProps Lite plugin dashboard.
- * php version 7.4.29
  *
- * @category Dashboard
- * @package  OxyPropsLite
  * @author   Cédric Bontems <dev@oxyprops.com>
- * @license  https://opensource.org/licenses/MIT MIT
- * @link     https://lite.oxyprops.com OxyProps Lite Website
  * @since    1.0.0
  */
-class Dashboard extends BaseController
-{
-    public $settings;
-    public $pages = [];
-    public $callbacks;
+class Dashboard extends BaseController {
 
-    /**
-     * Stores the Dashboard Singleton.
-     *
-     * @var object
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    private static $_instance;
+	/**
+	 * Admin settings API instance
+	 *
+	 * @var object
+	 *
+	 * @author   Cédric Bontems <dev@oxyprops.com>
+	 * @since    1.0.0
+	 */
+	public $settings;
 
-    /**
-     * Returns the Dashboard Singleton.
-     *
-     * @return object Instance
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public static function getInstance()
-    {
-        if (null === self::$_instance) {
-            self::$_instance = new Dashboard();
-        }
+	/**
+	 * Admin pages
+	 *
+	 * @var array
+	 *
+	 * @author   Cédric Bontems <dev@oxyprops.com>
+	 * @since    1.0.0
+	 */
+	public $pages = array();
 
-        return self::$_instance;
-    }
+	/**
+	 * Admin callbacks instance
+	 *
+	 * @var object
+	 *
+	 * @author   Cédric Bontems <dev@oxyprops.com>
+	 * @since    1.0.0
+	 */
+	public $callbacks;
 
-    /**
-     * Initializes the Dashboard Class
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public function register()
-    {
-        $this->settings = SettingsApi::getInstance();
-        $this->callbacks = AdminCallbacks::getInstance();
-        $this->callbacksFields = FieldsCallbacks::getInstance();
+	/**
+	 * Stores the Dashboard Singleton.
+	 *
+	 * @var object
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	private static $instance;
 
-        $this->setPages();
-        $this->setAdminSettings();
-        $this->setAdminSections();
-        $this->setAdminFields();
+	/**
+	 * Returns the Dashboard Singleton.
+	 *
+	 * @return object Instance
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public static function get_instance() {
+		if ( null === self::$instance ) {
+			self::$instance = new Dashboard();
+		}
 
-        $this->settings->addPages($this->pages)->withSubPage('Dashboard')
-            ->registerSetting();
+		return self::$instance;
+	}
 
-        // Redirect to about page after activation.
-        add_action('activated_plugin', [$this, 'redirect'], 10, 2);
-        add_action('load-toplevel_page_oxyprops_lite', [$this, 'enqueue'], 10, 2);
-    }
+	/**
+	 * Initializes the Dashboard Class
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public function register() {
+		$this->settings         = SettingsApi::get_instance();
+		$this->callbacks        = AdminCallbacks::get_instance();
+		$this->callbacks_fields = FieldsCallbacks::get_instance();
 
-    /**
-     * Defines plugin dashboard admin pages
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public function setPages()
-    {
-        $this->pages = [
-            [
-                'page_title' => __('OxyProps Lite', $this->textDomain),
-                'menu_title' => __('OxyProps Lite', $this->textDomain),
-                'capability' => 'manage_options',
-                'menu_slug' => 'oxyprops_lite',
-                'callback' => [$this->callbacks, 'adminDashboard'],
-                // 'callback' => [$this, 'render'],
-                'icon_url' => $this->activeIcon,
-                'position' => 110,
-            ],
-        ];
-    }
+		$this->setPages();
+		$this->setadmin_settings();
+		$this->setAdminSections();
 
-    /**
-     * Defines plugin dashboard admin settings
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public function setAdminSettings()
-    {
-        $args = [
-            [
-                'option_group' => 'oxyprops_lite_master_settings',
-                'option_name' => 'oxyprops_lite',
-                'callback' => [$this->callbacksFields, 'sanitizedCheckbox'],
-            ],
-        ];
-        $this->settings->addSettings($args);
-    }
+		// Wait for translations to be loaded.
+		add_action( 'init', array( $this, 'setAdminFields' ), 10, 2 );
 
-    /**
-     * Defines plugin dashboard admin sections
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public function setAdminSections()
-    {
-        $args = [
-            [
-                'id' => 'oxyprops_lite_master_settings_section',
-                'title' => null,
-                'callback' => '__return_null',
-                'page' => 'oxyprops_lite',
-            ],
-        ];
-        $this->settings->addSections($args);
-    }
+		$this->settings->add_pages( $this->pages )->with_sub_page( 'Dashboard' )
+			->register_setting();
 
-    /**
-     * Defines plugin dashboard admin fields
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public function setAdminFields()
-    {
-        $args = [];
-        foreach ($this->adminSettings as $key => $parameters) {
-            $args[] = [
-                'id' => $key,
-                'title' => $parameters['description'],
-                'callback' => [$this->callbacksFields, 'checkboxField'],
-                'page' => 'oxyprops_lite',
-                'section' => 'oxyprops_lite_master_settings_section',
-                'args' => [
-                    'option_name' => 'oxyprops_lite',
-                    'label_for' => $key,
-                    'class' => 'oxyprops-ui-toggle',
-                ],
-            ];
-        }
-        $this->settings->addFields($args);
-    }
+		// Redirect to about page after activation.
+		add_action( 'activated_plugin', array( $this, 'redirect' ), 10, 2 );
+		add_action( 'load-toplevel_page_oxyprops_lite', array( $this, 'enqueue' ), 10, 2 );
+	}
 
-    /**
-     * Functions and Hooks for Dashboard
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public function loadDashboard()
-    {
-        $this->enqueue();
-    }
+	/**
+	 * Defines plugin dashboard admin pages
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public function setPages() {
+		$this->pages = array(
+			array(
+				'page_title' => $this->adapted_name,
+				'menu_title' => $this->adapted_name,
+				'capability' => 'manage_options',
+				'menu_slug'  => 'oxyprops_lite',
+				'callback'   => array( $this->callbacks, 'admin_dashboard' ),
+				'icon_url'   => $this->active_icon,
+				'position'   => 110,
+			),
+		);
+	}
 
-    /**
-     * Enqueues Dashboard CSS and JS
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public function enqueue()
-    {
-        wp_enqueue_style(
-            'oxyprops-props',
-            $this->pluginUrl.'assets/css/open-props/open-props.op-lite.min.css',
-            [],
-            $this->version,
-            'all'
-        );
-        wp_enqueue_style(
-            'oxyprops-adminstyle',
-            $this->pluginUrl.'assets/css/dashboard.min.css',
-            [],
-            $this->version,
-            'all'
-        );
+	/**
+	 * Defines plugin dashboard admin settings
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public function setadmin_settings() {
+		$args = array(
+			array(
+				'option_group' => 'oxyprops_lite_master_settings',
+				'option_name'  => 'oxyprops_lite',
+				'callback'     => array( $this->callbacks_fields, 'sanitizedCheckbox' ),
+			),
+		);
+		$this->settings->add_settings( $args );
+	}
 
-        // enqueue all our scripts
-        wp_enqueue_script(
-            'oxyprops-adminscript',
-            $this->pluginUrl.'assets/js/dashboard.js',
-            [],
-            $this->version,
-            'all'
-        );
+	/**
+	 * Defines plugin dashboard admin sections
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public function setAdminSections() {
+		$args = array(
+			array(
+				'id'       => 'oxyprops_lite_master_settings_section',
+				'title'    => null,
+				'callback' => '__return_null',
+				'page'     => 'oxyprops_lite',
+			),
+		);
+		$this->settings->add_sections( $args );
+	}
 
-        add_filter('admin_footer_text', [$this, 'changeFooterText']);
-    }
+	/**
+	 * Defines plugin dashboard admin fields
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public function setAdminFields() {
+		$args             = array();
+		$default_settings = Options::plugin_options();
+		foreach ( $default_settings as $key => $parameters ) {
+			$args[] = array(
+				'id'       => $key,
+				'title'    => $parameters['description'],
+				'callback' => array( $this->callbacks_fields, 'checkboxField' ),
+				'page'     => 'oxyprops_lite',
+				'section'  => 'oxyprops_lite_master_settings_section',
+				'args'     => array(
+					'option_name' => 'oxyprops_lite',
+					'label_for'   => $key,
+					'class'       => 'oxyprops-ui-toggle',
+				),
+			);
+		}
+		$this->settings->add_fields( $args );
+	}
 
-    /**
-     * Customize WordPress Footer on the dashboard page
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public function changeFooterText()
-    {
-        // Translators: %1$s - link to review form.
-        echo wp_kses_post(
-            sprintf(
-                __(
-                    'Please rate <strong>OxyProps Lite</strong> '.
-                    '<a href="%1$s" target="_blank">&#9733;&#9733;&#9733;&#9733;'.
-                    '&#9733;</a> on <a href="%1$s" target="_blank">our Facebook Page'.
-                    '</a> to help us spread the word. Thank you from the OxyProps'.
-                    ' team!',
-                    $this->textDomain
-                ),
-                'https://www.facebook.com/oxyprops/reviews/'
-            )
-        );
-    }
+	/**
+	 * Functions and Hooks for Dashboard
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public function loadDashboard() {
+		$this->enqueue();
+	}
 
-    /**
-     * Redirect to dashboard after activation
-     *
-     * @return void
-     *
-     * @since  1.0.0
-     * @author Cédric Bontems <dev@oxyprops.com>
-     */
-    public function redirect()
-    {
-        wp_safe_redirect('admin.php?page=oxyprops_lite');
-        die;
-    }
+	/**
+	 * Enqueues Dashboard CSS and JS
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public function enqueue() {
+		wp_enqueue_style(
+			'oxyprops-props',
+			$this->plugin_url . 'assets/css/open-props/open-props.op-lite.min.css',
+			array(),
+			$this->version,
+			'all'
+		);
+		wp_enqueue_style(
+			'oxyprops-adminstyle',
+			$this->plugin_url . 'assets/css/dashboard.min.css',
+			array(),
+			$this->version,
+			'all'
+		);
+
+		// enqueue all our scripts.
+		wp_enqueue_script(
+			'oxyprops-adminscript',
+			$this->plugin_url . 'assets/js/dashboard.js',
+			array(),
+			$this->version,
+			'all'
+		);
+
+		add_filter( 'admin_footer_text', array( $this, 'changeFooterText' ) );
+	}
+
+	/**
+	 * Customize WordPress Footer on the dashboard page
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public function changeFooterText() {
+		echo wp_kses_post(
+			sprintf(
+				// Translators: %1$s - link to review form.
+				__(
+					'Please rate <strong>%2$s</strong> <a href="%1$s" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a> on <a href="%1$s" target="_blank">our Facebook Page</a> to help us spread the word. Thank you from the OxyProps team!',
+					'oxyprops_lite'
+				),
+				'https://www.facebook.com/oxyprops/reviews/',
+				$this->adapted_name
+			)
+		);
+	}
+
+	/**
+	 * Redirect to dashboard after activation
+	 *
+	 * @return void
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public function redirect() {
+		wp_safe_redirect( 'admin.php?page=oxyprops_lite' );
+		die;
+	}
 }
--- includes/Dashboard/IndividualPackages.php
+++ includes/Dashboard/IndividualPackages.php
@@ -1,10 +1,15 @@
 <?php
-
 /**
- * OxyProps Lite.
+ * Individual Packages
+ * Creates OxyProps Lite split individual packages.
+ * php version 7.4.29
  *
- * @see              https://lite.oxyprops.com
- * @since             1.0.0
+ * @category Dashboard
+ * @package  OxyPropsLite
+ * @author   Cédric Bontems <dev@oxyprops.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://lite.oxyprops.com OxyProps Lite Website
+ * @since    1.0.0
  */
 
 namespace Inc\Dashboard;
@@ -13,134 +18,208 @@ use Inc\Api\Callbacks\FieldsCallbacks;
 use Inc\Api\Callbacks\PackagesCallbacks;
 use Inc\Api\SettingsApi;
 use Inc\Base\BaseController;
+use Inc\Base\Options;
 
-class IndividualPackages extends BaseController
-{
-    public $settings;
-    public $subPages = [];
-    public $callbacks;
-    public $optionName = 'oxyprops_lite_bundle';
+/**
+ * IndividualPackages Class
+ * Creates OxyProps Lite split individual packages.
+ *
+ * @author   Cédric Bontems <dev@oxyprops.com>
+ * @since    1.0.0
+ */
+class IndividualPackages extends BaseController {
 
-    private static $instance;
+	/**
+	 * Admin settings API instance
+	 *
+	 * @var object
+	 *
+	 * @author   Cédric Bontems <dev@oxyprops.com>
+	 * @since    1.0.0
+	 */
+	public $settings;
 
-    public static function getInstance()
-    {
-        if (null === self::$instance) {
-            self::$instance = new IndividualPackages();
-        }
+	/**
+	 * Admin subpages
+	 *
+	 * @var array
+	 *
+	 * @author   Cédric Bontems <dev@oxyprops.com>
+	 * @since    1.0.0
+	 */
+	public $sub_pages = array();
 
-        return self::$instance;
-    }
+	/**
+	 * Admin callbacks instance
+	 *
+	 * @var object
+	 *
+	 * @author   Cédric Bontems <dev@oxyprops.com>
+	 * @since    1.0.0
+	 */
+	public $callbacks;
 
-    public function register()
-    {
-        if ($this->disabled($this->optionName)) {
-            return;
-        }
+	/**
+	 * Option name
+	 *
+	 * @var string
+	 *
+	 * @author   Cédric Bontems <dev@oxyprops.com>
+	 * @since    1.0.0
+	 */
+	public $option_name = 'oxyprops_lite_bundle';
 
-        $this->settings = SettingsApi::getInstance();
-        // $this->settings = new SettingsApi();
-        $this->callbacks = PackagesCallbacks::getInstance();
-        $this->callbacksFields = FieldsCallbacks::getInstance();
+	/**
+	 * Stores the IndividualPackages Singleton.
+	 *
+	 * @var object
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	private static $instance;
 
-        // $this->setSubPages();
-        $this->setPackagesSettings();
-        $this->setPackagesSections();
-        $this->setPackagesFields();
+	/**
+	 * Returns the IndividualPackages Singleton.
+	 *
+	 * @return object Instance
+	 *
+	 * @since  1.0.0
+	 * @author Cédric Bontems <dev@oxyprops.com>
+	 */
+	public static function get_instance() {
+		if ( null === self::$instance ) {
+			self::$instance = new IndividualPackages();
+		}
 
-        $this->settings->addSubPages($this->subPages)->registerSetting();
-    }
+		return self::$instance;
+	}
 
-    // public function setSubPages()
-    // {
-    //     $this->subPages = [
-    //         [
-    //             'parent_slug' => 'oxyprops_lite',
-    //             'page_title' => 'Individual Packages',
-    //             'menu_title' => 'Packages',
-    //             'capability' => 'manage_options',
-    //             'menu_slug' => 'oxyprops_lite_pkg',
-    //             'callback' => [$this->callbacks, 'packagesDashboard'],
-    //         ],
-    //     ];
-    // }
+	/**
+	 * IndividualPackages registration
+	 *
+	 * @return void
+	 *
+	 * @author   Cédric Bontems <dev@oxyprops.com>
+	 * @since    1.0.0
+	 */
+	public function register() {
+		if ( $this->disabled( $this->option_name ) ) {
+			return;
+		}
 
-    public function setPackagesSettings()
-    {
-        $args = [
-            [
-                'option_group' => 'oxyprops_lite_packages_settings1',
-                'option_name' => 'oxyprops_lite_packages',
-                'callback' => [$this->callbacksFields, 'sanitizedPackages'],
-            ],
-            [
-                'option_group' => 'oxyprops_lite_packages_settings2',
-                'option_name' => 'oxyprops_lite_packages',
-                'callback' => [$this->callbacksFields, 'sanitizedPackages'],
-            ],
-            [
-                'option_group' => 'oxyprops_lite_packages_settings3',
-                'option_name' => 'oxyprops_lite_packages',
-                'callback' => [$this->callbacksFields, 'sanitizedPackages'],
-            ],
-        ];
-        $this->settings->addSettings($args);
-    }
+		$this->settings         = SettingsApi::get_instance();
+		$this->callbacks        = PackagesCallbacks::get_instance();
+		$this->callbacks_fields = FieldsCallbacks::get_instance();
 
-    public function setPackagesSections()
-    {
-        $args = [
-            [
-                'id' => 'oxyprops_lite_main_packages_section',
-                'title' => null,
-                'callback' => '__return_null',
-                'page' => 'oxyprops_lite_pkg1',
-            ],
-            [
-                'id' => 'oxyprops_lite_individual_colors_section',
-                'title' => null,
-                'callback' => '__return_null',
-                'page' => 'oxyprops_lite_pkg2',
-            ],
-            [
-                'id' => 'oxyprops_lite_individual_colors_hsl_section',
-                'title' => null,
-                'callback' => '__return_null',
-                'page' => 'oxyprops_lite_pkg3',
-            ],
-        ];
-        $this->settings->addSections($args);
-    }
+		$this->set_packages_settings();
+		$this->set_packages_sections();
+		// $this->set_packages_fields();
 
-    public function setPackagesFields()
-    {
-        $args = [];
-        foreach ($this->packagesSettings as $key => $parameters) {
-            $column = '';
-            switch ($parameters['section']) {
-            case 'main_packages':
-                $column = '1';
-                break;
-            case 'individual_colors':
-                $column = '2';
-                break;
-            case 'individual_colors_hsl':
-                $column = '3';
-                break;
-            }
-            $args[] = [
-                'id' => $key,
-                'title' => $parameters['description'],
-                'callback' => [$this->callbacksFields, 'checkboxField'],
-                'page' => 'oxyprops_lite_pkg'.$column,
-                'section' => 'oxyprops_lite_'.$parameters['section'].'_section',
-                'args' => [
-                    'option_name' => 'oxyprops_lite_packages',
-                    'label_for' => $key,
-                    'class' => 'oxyprops-ui-toggle',
-                ],
-            ];
-        }
-        $this->settings->addFields($args);
-    }
+		// Wait for translations to be loaded.
+		add_action( 'init', array( $this, 'set_packages_fields' ), 10, 2 );
+
+		$this->settings->add_sub_pages( $this->sub_pages )->register_setting();
+	}
+
+	/**
+	 * IndividualPackages settings registration
+	 *
+	 * @return void
+	 *
+	 * @author   Cédric Bontems <dev@oxyprops.com>
+	 * @since    1.0.0
+	 */
+	public function set_packages_settings() {
+		$args = array(
+			array(
+				'option_group' => 'oxyprops_lite_packages_settings1',
+				'option_name'  => 'oxyprops_lite_packages',
+				'callback'     => array( $this->callbacks_fields, 'sanitizedPackages' ),
+			),
+			array(
+				'option_group' => 'oxyprops_lite_packages_settings2',
+				'option_name'  => 'oxyprops_lite_packages',
+				'callback'     => array( $this->callbacks_fields, 'sanitizedPackages' ),
+			),
+			array(
+				'option_group' => 'oxyprops_lite_packages_settings3',
+				'option_name'  => 'oxyprops_lite_packages',
+				'callback'     => array( $this->callbacks_fields, 'sanitizedPackages' ),
+			),
+		);
+		$this->settings->add_settings( $args );
+	}
+
+	/**
+	 * IndividualPackages sections registration
+	 *
+	 * @return void
+	 *
+	 * @author   Cédric Bontems <dev@oxyprops.com>
+	 * @since    1.0.0
+	 */
+	public function set_packages_sections() {
+		$args = array(
+			array(
+				'id'       => 'oxyprops_lite_main_packages_section',
+				'title'    => null,
+				'callback' => '__return_null',
+				'page'     => 'oxyprops_lite_pkg1',
+			),
+			array(
+				'id'       => 'oxyprops_lite_individual_colors_section',
+				'title'    => null,
+				'callback' => '__return_null',
+				'page'     => 'oxyprops_lite_pkg2',
+			),
+			array(
+				'id'       => 'oxyprops_lite_individual_colors_hsl_section',
+				'title'    => null,
+				'callback' => '__return_null',
+				'page'     => 'oxyprops_lite_pkg3',
+			),
+		);
+		$this->settings->add_sections( $args );
+	}
+
+	/**
+	 * IndividualPackages fields registration
+	 *
+	 * @return void
+	 *
+	 * @author   Cédric Bontems <dev@oxyprops.com>
+	 * @since    1.0.0
+	 */
+	public function set_packages_fields() {
+		$args             = array();
+		$default_settings = Options::packages_options();
+		foreach ( $default_settings as $key => $parameters ) {
+			$column = '';
+			switch ( $parameters['section'] ) {
+				case 'main_packages':
+					$column = '1';
+					break;
+				case 'individual_colors':
+					$column = '2';
+					break;
+				case 'individual_colors_hsl':
+					$column = '3';
+					break;
+			}
+			$args[] = array(
+				'id'       => $key,
+				'title'    => $parameters['description'],
+				'callback' => array( $this->callbacks_fields, 'checkboxField' ),
+				'page'     => 'oxyprops_lite_pkg' . $column,
+				'section'  => 'oxyprops_lite_' . $parameters['section'] . '_section',
+				'args'     => array(
+					'option_name' => 'oxyprops_lite_packages',
+					'label_for'   => $key,
+					'class'       => 'oxyprops-ui-toggle',
+				),
+			);
+		}
+		$this->settings->add_fields( $args );
+	}
 }
